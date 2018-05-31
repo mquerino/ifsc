e4325f4e11d93a3f791db196856ef15f2f964946
Marcos Fidelis Querino <38235474+mquerino@users.noreply.github.com>
2018-05-31 19:04:04 -0300

    página principal
    arquivos para dar incio navegacao

---
+++ laguna.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       echo 'OLA LAGUNA !!!!';
+        ?><P>
+        <a href=cidade.html>VOLTAR</a>
+    </body>
+</html>
